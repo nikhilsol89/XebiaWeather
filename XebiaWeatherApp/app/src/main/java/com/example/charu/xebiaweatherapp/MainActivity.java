ddee913d35fbcd2306ced0740d61bfa9837364d7
nikhilsol89 <nikhilschumifanikhil@gmail.com>
2016-06-26 15:44:03 +0530

    integrated volley app

---
+++ XebiaWeatherApp/app/src/main/java/com/example/charu/xebiaweatherapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.charu.xebiaweatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ XebiaWeatherApp/app/src/main/java/com/example/charu/xebiaweatherapp/application/XebiaWeatherMainApplication.java
@@ -0,0 +1,46 @@
+package com.example.charu.xebiaweatherapp.application;
+
+import android.app.Application;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Charu on 6/26/2016.
+ */
+public class XebiaWeatherMainApplication extends Application {
+
+    String tagForRequestQueue = XebiaWeatherMainApplication.class.getSimpleName();
+    RequestQueue volleyrequestQueue;
+    private static XebiaWeatherMainApplication mainApplication;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mainApplication= this;
+    }
+
+    public RequestQueue getRequestQueue(){
+        if(volleyrequestQueue ==null){
+            volleyrequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+        return volleyrequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> request,String tag){
+        request.setTag(tag==null?tagForRequestQueue:tag);
+        getRequestQueue().add(request);
+    }
+
+    public <T> void addToRequestQueue(Request<T> request){
+        request.setTag(tagForRequestQueue);
+        getRequestQueue().add(request);
+    }
+
+    public void cancelPendingRequest(Object tag){
+        if(volleyrequestQueue!=null){
+            volleyrequestQueue.cancelAll(tag);
+        }
+    }
+}
